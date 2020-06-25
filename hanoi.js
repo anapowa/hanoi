092ee61f909c0d149d32cf0e99971da337a6b5e4
Ana Ruiz Tovar <anapowa@gmail.com>
2020-06-25 01:16:33 -0400

    Added "selected" class to selectedDisc to visually identify the current movable piece.

--- hanoi.js
+++ hanoi.js
@@ -10,6 +10,7 @@ const clickTower = function (evt) {
         if(disc){
             console.log("you picked up  " + disc.id)
             selectedDisc = disc
+            selectedDisc.classList.add("selected")
             mode = "drop"
         } else {
             console.log("no disc to pick up")
@@ -17,31 +18,26 @@ const clickTower = function (evt) {
     } else if (mode === "drop") {
         //drop
         console.log("You dropped " + selectedDisc.id + " onto " + tower.id + " " + selectedDisc.dataset.width)
-        // tower.append(selectedDisc)
         mode = "pickup"
         if (!event.currentTarget.firstElementChild){
             tower.append(selectedDisc)
+            selectedDisc.classList.remove("selected")
         } else if (selectedDisc.dataset.width > tower.firstElementChild.dataset.width) {
             console.log("Yay")
             tower.append(selectedDisc)
+            selectedDisc.classList.remove("selected")
         } else {
             alert ("Invalid move! Disc can't be placed on top of a smaller one. Please place on a different tower.")
             mode = "drop"
         }
     }
-        if (tower3.childElementCount === 4)
-        alert ("You won!!!")
+    if (tower3.childElementCount === 4)
+    alert ("You won!!!")
 }
-//if (selectedDisc.dataset.width < (disc.dataset.width || tower.dataset.width))
-// (selectedDisc.dataset.width < disc.dataset.width)
 
 const tower1 = document.querySelector("#tower1")
 const tower2 = document.querySelector("#tower2")
 const tower3 = document.querySelector("#tower3")
-let disc1 = document.querySelector("#disc1")
-let disc2 = document.querySelector("#disc2")
-let disc3 = document.querySelector("#disc3")
-let disc4 = document.querySelector("#disc4")
 
 tower1.addEventListener("click", clickTower)
 tower2.addEventListener("click", clickTower)
