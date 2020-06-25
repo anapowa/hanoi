17c5c1bb9310d6ba2874ea530f7fa0958d239580
Ana Ruiz Tovar <anapowa@gmail.com>
2020-06-25 00:16:47 -0400

    Added additional functionality to drop a disc if section is empty and condition to drop disc if section has a disc to ensure the disc can only be dropped if it's smaller than the one currently on top of  the tower. (Based on study hall session).
    Added winning condition to let user know when the game has been completed.

--- hanoi.js
+++ hanoi.js
@@ -1,27 +1,39 @@
-
+//Included code from Randy's demo and study hall on 24JUN2020.
 
 let mode = "pickup"
 let selectedDisc = null
 
-const grabDisc = function (evt) {
+const clickTower = function (evt) {
     const tower = evt.currentTarget
+    const disc = tower.lastElementChild
     if (mode === "pickup") {
-        //pickup
-        const disc = tower.lastElementChild
         if(disc){
-            console.log("you picked up disc " + disc.id)
+            console.log("you picked up  " + disc.id)
             selectedDisc = disc
             mode = "drop"
         } else {
             console.log("no disc to pick up")
         }
-    } else {
+    } else if (mode === "drop") {
         //drop
-        console.log("You dropped onto " + tower.id)
-        tower.append(selectedDisc)
+        console.log("You dropped " + selectedDisc.id + " onto " + tower.id + " " + selectedDisc.dataset.width)
+        // tower.append(selectedDisc)
         mode = "pickup"
+        if (!event.currentTarget.firstElementChild){
+            tower.append(selectedDisc)
+        } else if (selectedDisc.dataset.width > tower.firstElementChild.dataset.width) {
+            console.log("Yay")
+            tower.append(selectedDisc)
+        } else {
+            alert ("Invalid move! Disc can't be placed on top of a smaller one. Please place on a different tower.")
+            mode = "drop"
+        }
     }
+        if (tower3.childElementCount === 4)
+        alert ("You won!!!")
 }
+//if (selectedDisc.dataset.width < (disc.dataset.width || tower.dataset.width))
+// (selectedDisc.dataset.width < disc.dataset.width)
 
 const tower1 = document.querySelector("#tower1")
 const tower2 = document.querySelector("#tower2")
@@ -31,6 +43,7 @@ let disc2 = document.querySelector("#disc2")
 let disc3 = document.querySelector("#disc3")
 let disc4 = document.querySelector("#disc4")
 
-tower1.addEventListener("click", grabDisc)
-tower2.addEventListener("click", grabDisc)
-tower3.addEventListener("click", grabDisc)
+tower1.addEventListener("click", clickTower)
+tower2.addEventListener("click", clickTower)
+tower3.addEventListener("click", clickTower)
+
