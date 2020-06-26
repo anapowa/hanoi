64339d5af7901c0ddd8b644fe7b92db0dd056d0c
Ana Ruiz Tovar <anapowa@gmail.com>
2020-06-25 20:41:19 -0400

    Fixed bug on code JS that allowed bigger disc to be stacked on top of smaller disc on left and middle column.

--- hanoi.js
+++ hanoi.js
@@ -6,6 +6,7 @@ let selectedDisc = null
 const clickTower = function (evt) {
     const tower = evt.currentTarget
     const disc = tower.lastElementChild
+
     if (mode === "pickup") {
         if(disc){
             console.log("you picked up  " + disc.id)
@@ -19,11 +20,11 @@ const clickTower = function (evt) {
         //drop
         console.log("You dropped " + selectedDisc.id + " onto " + tower.id + " " + selectedDisc.dataset.width)
         mode = "pickup"
-        if (!event.currentTarget.firstElementChild){
+        if (!evt.currentTarget.lastElementChild){
             tower.append(selectedDisc)
             selectedDisc.classList.remove("selected")
-        } else if (selectedDisc.dataset.width > tower.firstElementChild.dataset.width) {
-            console.log("Yay")
+        } else if (selectedDisc.dataset.width < tower.lastElementChild.dataset.width) {
+            console.log("dropped :) ")
             tower.append(selectedDisc)
             selectedDisc.classList.remove("selected")
         } else {
@@ -36,10 +37,10 @@ const clickTower = function (evt) {
 }
 
 const tower1 = document.querySelector("#tower1")
-const tower2 = document.querySelector("#tower2")
-const tower3 = document.querySelector("#tower3")
-
 tower1.addEventListener("click", clickTower)
-tower2.addEventListener("click", clickTower)
-tower3.addEventListener("click", clickTower)
 
+const tower2 = document.querySelector("#tower2")
+tower2.addEventListener("click", clickTower)
+
+const tower3 = document.querySelector("#tower3")
+tower3.addEventListener("click", clickTower)
